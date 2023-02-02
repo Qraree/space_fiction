5dfc5a7598d2cff30043e211d52e62fe656fa0dd
Dmitry Lysakov <dmitry.ly@fora-soft.com>
2023-02-02 19:41:18 +0300

    feat: home components

---
+++ frontend/src/containers/Home/RocketScienceHistory/HomeRocketScienceHistory.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HomeRocketScienceHistory.module.scss'
+
+const HomeRocketScienceHistory = () => {
+    return (
+        <div className={styles.historyContainer}>
+
+        </div>
+    );
+};
+
+export default HomeRocketScienceHistory;
---
+++ frontend/src/containers/Home/StarSky/HomeStarSky.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './HomeStarSky.module.scss'
+
+const HomeStarSky = () => {
+
+    return (
+        <div className={styles.starSkyContainer}>
+
+        </div>
+    );
+};
+ 
+export default HomeStarSky;
