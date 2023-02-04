8b7970fd3c2ad62f0aafa135bc950c2649cef5af
Qraree <metbezu@gmail.com>
2023-02-04 16:17:14 +0300

    feat: implemented canvas star sky

--- frontend/src/containers/Home/RocketScienceHistory/HomeRocketScienceHistory.tsx
+++ frontend/src/containers/Home/RocketScienceHistory/HomeRocketScienceHistory.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import styles from './HomeRocketScienceHistory.module.scss'
+import React, {FC, useEffect, useState} from 'react';
+import styles from './HomeRocketScienceHistory.module.scss';
+import Navbar from '@/components/Navbar/Navbar';
 
-const HomeRocketScienceHistory = () => {
+const HomeRocketScienceHistory: FC = () => {
+    const [navbarHidden, setNavbarHidden] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', function() {
+            if (document.body.scrollTop > innerHeight - 100 || document.documentElement.scrollTop > innerHeight - 100) {
+                console.log('showNavbar');
+                setNavbarHidden(true);
+            } else {
+                setNavbarHidden(false);
+                console.log('hideNavbar');
+            }
+        });
+    }, []);
+
     return (
         <div className={styles.historyContainer}>
-
+            <Navbar navbarHidden={navbarHidden} />
         </div>
     );
 };
--- frontend/src/containers/Home/StarSky/HomeStarSky.tsx
+++ frontend/src/containers/Home/StarSky/HomeStarSky.tsx
@@ -1,11 +1,96 @@
-import React from 'react';
-import styles from './HomeStarSky.module.scss'
+import React, {FC, useEffect, useRef} from 'react';
+import styles from './HomeStarSky.module.scss';
+import {randomChoice, randomInInterval, randomInIntervalFloor, randomNumber} from '@/helpers/random';
+import useDeviceSize from '@/hooks/useDeviceSize';
 
-const HomeStarSky = () => {
+
+const HomeStarSky: FC = () => {
+    const [widthState, heightState] = useDeviceSize();
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        // @ts-ignore
+        const ctx = canvas.getContext('2d');
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+
+        function Circle() {
+            this.x = randomNumber(innerWidth);
+            this.y = randomNumber(innerHeight);
+            this.dx = randomInInterval(-0.05, 0.05);
+            this.dy = randomInInterval(-0.05, 0.05);
+            this.radius = randomNumber(1.5);
+            this.opacity = randomInInterval(0, 1);
+
+            this.draw = function () {
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+                ctx.strokeStyle = 'black';
+                ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity}`;
+                ctx.stroke();
+                ctx.fill();
+
+            };
+
+            this.update = function () {
+                if (this.x + this.radius > innerWidth || this.y - this.radius < 0) {
+                    this.x = randomChoice(0, randomNumber(innerWidth));
+                    if (this.x === 0) {
+                        this.y = randomNumber(innerHeight);
+                    } else {
+                        this.y = innerHeight;
+                    }
+                }
+
+                if (this.x - this.radius < 0 || this.y + this.radius > innerHeight) {
+                    this.x = randomChoice(innerWidth, randomNumber(innerWidth));
+                    if (this.x === innerWidth) {
+                        this.y = randomNumber(innerHeight);
+                    } else {
+                        this.y = 0;
+                    }
+                }
+                this.x += this.dx;
+                this.y += this.dy;
+                this.draw();
+            };
+        }
+
+        const circleArray: any = [];
+        
+        for (let i = 0; i < 2000; i++) {
+            // @ts-ignore
+            circleArray.push(new Circle());
+        }
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+            for (let i = 0; i < circleArray.length; i++) {
+                circleArray[i].update();
+            }
+        };
+
+
+        animate();
+
+    }, []);
+
+    const handleClick = () => {
+        window.scrollTo({
+            top: innerHeight,
+            behavior: 'smooth'
+        });
+    };
 
     return (
         <div className={styles.starSkyContainer}>
-
+            <canvas ref={canvasRef} />
+            <button
+                className={styles.button}
+                onClick={handleClick}>Click</button>
         </div>
     );
 };
