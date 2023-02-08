6727f7f16616dcd2b6f7ecfd31cd2ccdca44039e
Qraree <metbezu@gmail.com>
2023-02-08 19:03:22 +0300

    fix: fixed pages layout

--- frontend/src/pages/rockets.tsx
+++ frontend/src/pages/rockets.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PageHeader from '@/components/common/PageHeader/PageHeader';
+import Navbar from '@/components/Navbar/Navbar';
 
 const Rockets = () => {
     return (
         <>
             <PageHeader />
-            <div className="container">
-
-            </div>
+            <Navbar />
         </>
     );
 };
