40c1460cd48696eb1c0a2340e2052986d87f846b
Qraree <metbezu@gmail.com>
2023-02-11 17:17:53 +0300

    feat: implemented rocket section

--- frontend/src/pages/rockets.tsx
+++ frontend/src/pages/rockets.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PageHeader from '@/components/common/PageHeader/PageHeader';
 import Navbar from '@/components/Navbar/Navbar';
+import RocketsSection from '@/containers/Rockets/RocketsSection';
 
 const Rockets = () => {
     return (
         <>
             <PageHeader />
             <Navbar />
+            <RocketsSection />
         </>
     );
 };
