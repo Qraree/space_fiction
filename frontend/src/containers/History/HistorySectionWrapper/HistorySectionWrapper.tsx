f9934ba2d65fa26bf503d9118e906321007a10b6
Qraree <metbezu@gmail.com>
2023-02-12 19:14:14 +0300

    refactoring: made a wrapper component for article

--- frontend/src/containers/History/HistorySectionWrapper/HistorySectionWrapper.tsx
+++ frontend/src/containers/History/HistorySectionWrapper/HistorySectionWrapper.tsx
@@ -3,6 +3,7 @@ import styles from './HistorySectionWrapper.module.scss';
 import {HISTORY_ARTICLES} from '@/constants/history';
 import LinkStyled from '@/components/common/LinkStyled/LinkStyled';
 import {useRouter} from 'next/router';
+import Sidebar from '@/components/Sidebar/Sidebar';
 
 type HistorySectionProps = {
     children?: React.ReactNode
@@ -16,23 +17,7 @@ const HistorySectionWrapper = ({ children }: HistorySectionProps) => {
             <div className={styles.sectionNavigationWrapper}>
                 <div className={styles.sectionNavigation}>
                     <h4>History Articles</h4>
-                    {HISTORY_ARTICLES.map( article => (
-                        <LinkStyled href={article.link} key={article.id}>
-                            <div
-                                className={styles.articleNav}
-                                style={{
-                                    background: article.link === router.asPath
-                                        ? 'white'
-                                        : 'transparent',
-                                    color: article.link === router.asPath
-                                        ? 'black'
-                                        : 'white',
-                                }}
-                            >
-                                {article.title}
-                            </div>
-                        </LinkStyled>
-                    ))}
+                    <Sidebar />
                 </div>
             </div>
             <div className={styles.wrapper}>
---
+++ frontend/src/containers/Rockets/RocketArticleWrapper/RocketArticleWrapper.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './RocketArticleWrapper.module.scss';
+
+type RocketArticleWrapperProps = {
+    children?: React.ReactNode;
+}
+
+const RocketArticleWrapper = ({children}: RocketArticleWrapperProps) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default RocketArticleWrapper;
