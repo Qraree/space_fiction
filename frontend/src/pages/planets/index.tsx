683f47d95242c507206873fe04f1455098b16be1
Qraree <metbezu@gmail.com>
2023-02-16 13:39:59 +0300

    feat: planets section

---
+++ frontend/src/pages/planets/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PageHeader from '@/components/common/PageHeader/PageHeader';
+import Navbar from '@/components/Navbar/Navbar';
+import RocketArticleWrapper from '@/containers/Rockets/RocketArticleWrapper/RocketArticleWrapper';
+import PlanetsSection from '@/containers/Planets/PlanetsSection';
+
+const Index = () => {
+    return (
+        <>
+            <PageHeader />
+            <Navbar />
+            <RocketArticleWrapper>
+                <PlanetsSection />
+            </RocketArticleWrapper>
+        </>
+    );
+};
+
+export default Index;
