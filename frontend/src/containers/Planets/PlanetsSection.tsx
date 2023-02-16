157970e7286c7c8af2a67422bf2557e30e103264
Qraree <metbezu@gmail.com>
2023-02-16 13:38:31 +0300

    feat: planets section

--- frontend/src/containers/Planets/PlanetsSection.tsx
+++ frontend/src/containers/Planets/PlanetsSection.tsx
@@ -1,22 +1,62 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import styles from './PlanetsSection.module.scss';
-import { Canvas } from '@react-three/fiber';
-import Model from '@/components/models/planets/Model';
-import {Environment, OrbitControls} from '@react-three/drei';
+import LinkStyled from '@/components/common/LinkStyled/LinkStyled';
+
+const MOCK_PLANETS = [
+    {
+        title: 'Earth',
+        img: '/img/tabPhotos/planets_tab_photo.jpg',
+        description: 'fwehowheuweh fwefwefwe wlqpl[p,q'
+    },
+    {
+        title: 'Mars',
+        img: '/img/tabPhotos/planets_tab_photo.jpg',
+        description: 'fwehowqd ;pl[l[pf wqdiqw jdqw hudwhq hdoiqw'
+    },
+    {
+        title: 'Venus',
+        img: '/img/tabPhotos/planets_tab_photo.jpg',
+        description: 'qw wqpdokoqpw po wdwq pp'
+    },
+    {
+        title: 'Saturn',
+        img: '/img/tabPhotos/planets_tab_photo.jpg',
+        description: 'opdkq pwwqjdpqw qw djpq wdo jqw jdqw jdqw jdpq wjdjjj dqwojwoq djqwjd  oj qw po'
+    },
+    {
+        title: 'Jupyter',
+        img: '/img/tabPhotos/planets_tab_photo.jpg',
+        description: 'fwef ewfwqwd qi jdqow'
+    },
+
+];
+
 
 const PlanetsSection = () => {
     return (
-        <div className={styles.planetsContainer}>
-            <div className={styles.planetWrapper}>
-                <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-                    <Suspense fallback={null}>
-                        <Model />
-                        <Environment preset="city" />
-                    </Suspense>
-                    <OrbitControls autoRotate />
-                </Canvas>
-            </div>
-        </div>
+        <>
+            <h1>Planets</h1>
+            <hr />
+            <br />
+            {MOCK_PLANETS.map( planet => (
+                <LinkStyled href={`/planets/${planet.title}`}  key={planet.title}>
+                    <div className={styles.planetContainer}>
+                        <div className={styles.planetPhoto}>
+                            <img src={planet.img} />
+                        </div>
+                        <div className={styles.planetInfo}>
+                            <h2>{planet.title}</h2>
+                            <div className={styles.planetDescription}>
+                                <p>
+                                    {planet.description}
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </LinkStyled>
+            ))}
+        </>
     );
 };
 
---
+++ frontend/src/pages/planets/[planet].tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PageHeader from '@/components/common/PageHeader/PageHeader';
+import Navbar from '@/components/Navbar/Navbar';
+import RocketArticleWrapper from '@/containers/Rockets/RocketArticleWrapper/RocketArticleWrapper';
+import RocketArticle from '@/containers/Rockets/RocketArticle/RocketArticle';
+
+const Planet = () => {
+    return (
+        <>
+            <PageHeader />
+            <Navbar />
+            <RocketArticleWrapper>
+                <h1>Hello</h1>
+            </RocketArticleWrapper>
+        </>
+    );
+};
+
+export default Planet;
