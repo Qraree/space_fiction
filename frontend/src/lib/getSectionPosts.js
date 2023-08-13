0f6710127533b8e138a9a606b10c3f2caf364892
Qraree <metbezu@gmail.com>
2023-08-13 19:08:28 +0300

    Internationalization

---
+++ frontend/src/lib/getSectionPosts.js
@@ -0,0 +1,31 @@
+import { remark } from 'remark';
+import html from 'remark-html';
+import * as fs from 'fs';
+import matter from 'gray-matter';
+
+export async function getSectionPosts(section, language, rocket, locale) {
+  console.log(`/app/src/content/${section}/${String(locale)}/${rocket}.md`);
+  try {
+    const fileContents = fs.readFileSync(
+      `/app/src/content/${section}/${locale}/${rocket}.md`,
+      'utf8',
+    );
+
+    const matterResult = matter(fileContents);
+
+    const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+
+    // Combine the data with the id and contentHtml
+    return {
+      rocket,
+      contentHtml,
+      ...matterResult.data,
+    };
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
